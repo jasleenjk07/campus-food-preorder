e0a00b23f02b10d70761600a1a49a72451234f73
jasleenjk07 <kaurjasleen6104@gmail.com>
2026-02-10 22:21:25 +0530

    Add FoodItem and Order models to the database schema, including relationships and default values for attributes.

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,9 @@
 #Database Tables
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Boolean, DateTime
 from sqlalchemy.orm import relationship
 from .database import Base
+from datetime import datetime
 
 class User(Base):
     __tablename__ = "users"
@@ -22,3 +23,33 @@ class User(Base):
  # role TEXT NOT NULL,
  # university_id INTEGER
 #);
+
+class FoodItem(Base):
+    __tablename__ = "food_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    price = Column(Float, nullable=False)
+    is_available = Column(Boolean, default=True)
+
+    vendor_id = Column(Integer, ForeignKey("users.id"))
+    vendor = relationship("User")
+    
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id")) #This order belongs to a user from the users table
+    food_id = Column(Integer, ForeignKey("food_items.id")) #This links orders → food_items
+    quantity = Column(Integer, default=1) #Default is 1 if user doesn’t specify quantity
+    total_price = Column(Float)
+
+    status = Column(String, default="PLACED")
+    is_paid = Column(Boolean, default=False)
+    payment_method = Column(String, nullable=True)
+    
+    created_at = Column(DateTime, default=datetime.utcnow) #gives current time (UTC) and automatically stores when the order was created
+
+    user = relationship("User")
+    food = relationship("FoodItem")
