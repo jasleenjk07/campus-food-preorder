4daa5b6fa2b54718a28101b24320b4bbb75b901e
jasleenjk07 <kaurjasleen6104@gmail.com>
2026-02-10 22:20:17 +0530

    Add additional routers for users, admin, menu, and orders to the FastAPI application in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,8 @@
 #Connect Database to app
-print(">>> RUNNING backend/app/main.py <<<")
 from fastapi import FastAPI
 from app.database import engine
 from app import models
-from app.routers import auth
+from app.routers import auth, users, admin, menu, orders
 
 app = FastAPI(title="Campus Food Pre-Order API") #Creates FastAPI application object
 
@@ -11,6 +10,10 @@ app = FastAPI(title="Campus Food Pre-Order API") #Creates FastAPI application ob
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(admin.router)
+app.include_router(menu.router)
+app.include_router(orders.router) #Registers all order routes with your FastAPI app.
 
 #A simple test API 
 @app.get("/")
