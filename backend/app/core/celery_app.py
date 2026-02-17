b93016b784bd9bb7e3fbc180a9b84f31207ab5a6
jasleenjk07 <kaurjasleen6104@gmail.com>
2026-02-17 12:33:58 +0530

    Refactor Celery app configuration to utilize settings for Redis URL and specify task routing for send_notification_task

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -1,14 +1,14 @@
 from celery import Celery
+from app.config import settings
 
 celery_app = Celery(
     "eatsy", #Celery app name
-    broker="redis://localhost:6379/0", #message queue system. Redis running on:, Host: localhost, Port: 6379, Database: 0
-    backend="redis://localhost:6379/0",
-    include=["app.tasks.notifications"]
+    broker=settings.REDIS_URL,
+    backend=settings.REDIS_URL
 )
 
 celery_app.autodiscover_tasks(["app.tasks"])
 
 celery_app.conf.task_routes = { #Any task inside app.tasks.notifications should go to the notifications queue.
-    "app.tasks.notifications.*": {"queue": "notifications"}
+    "app.tasks.notifications.send_notification_task": {"queue": "notifications"}
 }
