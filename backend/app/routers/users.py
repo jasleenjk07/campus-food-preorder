ba5171b888b22d89b0ac56fe3c03dd7e406be8d8
jasleenjk07 <kaurjasleen6104@gmail.com>
2026-03-11 00:51:12 +0530

    Refactor user profile endpoint to return UserPublic or VendorPublic schemas based on user role. This enhances the API's response flexibility for different user types.

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, Depends
 from app.auth.deps import get_current_user #JWT verification logic
-from app.schemas import UserResponse
+from app.schemas import UserPublic, VendorPublic
 from app import models
 
 router = APIRouter(tags=["Users"])
 
-@router.get("/me", response_model=UserResponse)
-def get_my_profile(
-    current_user: models.User = Depends(get_current_user)
-):
-    return current_user
+@router.get("/me")
+def get_current_user_details(current_user = Depends(get_current_user)):
+
+    if current_user.role == "VENDOR":
+        return VendorPublic.from_orm(current_user)
+
+    return UserPublic.from_orm(current_user)
